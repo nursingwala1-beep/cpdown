1ec9b03dd9a8d7b906b014675d3d37d31e7f2a5c
nursingwala1-beep <nursingwala1@gmail.com>
2025-11-16 09:44:27 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
-bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
+api_id = "33021095" #change_with_your_Api_id.
+api_hash = "81e4f1f4a439dadff7aa2bdc808ead5f" #change_with_your_Api_hash.
+bot_token = "8346605241:AAGGvGJkNd6L1ukDTnWNV8_ig_c6BFVuXSk" #change_with_your_bot_token.
+auth_users = [1051495367] #change_with_your_user_id.
 sudo_user = 1713924419 #change_with_your_user_id.
 log_channel = -1002154281532 #change_with_your_channel_id.
 txt_channel = -1002154281532 #change_with_your_channel_id.
+
